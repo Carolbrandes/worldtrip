e6bc0a9f67527b800afb96cf17db14b53c090e3d
carol <carolbrandes23@gmail.com>
2023-09-06 07:22:15 -0300

    config of chackra theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      400: "#FFBA08",
+    },
+    gray: {
+      "700": "#47585B",
+      "100": "#DADADA",
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+        color: "gray.700",
+      },
+    },
+  },
+});
